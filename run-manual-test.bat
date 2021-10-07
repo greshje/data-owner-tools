f33a69af82f1f855e3c87036f043b9e041aa6eb1
unknown <greshje.nachc@gmail.com>
2021-10-07 19:16:43 -0400

    added test files an .bat file to run tests

--- run-manual-test.bat
+++ run-manual-test.bat
@@ -2,7 +2,9 @@
 echo.
 echo.
 echo Starting manual test for garble.py...
+@echo on
 python garble.py ./test/manual/inbox/pii.csv ./test/manual/schema ./test/manual/deidentification_secret.txt
+@echo off
 echo.
 echo Done.  
 echo.
